d097b536971a8e0d9a0e3b9af7a4bc37d9f8a142
Vinffs <vin.demiranda@gmail.com>
2023-12-21 16:21:30 +0100

    created table and filled it with information from Faker

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+
+        for ($i = 0; $i < 10; $i++) {
+
+            $train = new Train();
+            $train->company = $faker->company();
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
+            $train->departure_time = $faker->time();
+            $train->arrival_time = $faker->time();
+            $train->train_code = $faker->ean13();
+            $train->number_of_carriages = $faker->numberBetween(1, 20);
+            $train->is_on_time = $faker->boolean();
+            $train->is_deleted = $faker->boolean();
+            $train->save();
+        }
+    }
+}
