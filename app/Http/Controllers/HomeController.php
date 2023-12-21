d1f89b5d5f9dba4d976ce60c65a40247c2d7582a
Vinffs <vin.demiranda@gmail.com>
2023-12-21 17:10:48 +0100

    printed info about trains

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Train;
+
+class HomeController extends Controller
+{
+    public function home()
+    {
+        $train = Train::all();
+        return view("home", compact("train"));
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,38 @@
 @section('title', 'Home')
 
 @section('content')
-<main>
-    <h1>Home page</h1>
-</main>
+    <main class="py-5">
+        <h1 class="text-center">Departures</h1>
+        <div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Company</th>
+                        <th scope="col">Departure Station</th>
+                        <th scope="col">Departure Date</th>
+                        <th scope="col">Departure Time</th>
+                        <th scope="col">Arrival Station</th>
+                        <th scope="col">Arrival Date</th>
+                        <th scope="col">Arrival Time</th>
+                        <th scope="col">Train Code</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($train as $item)
+                        <tr>
+                            <th scope="row">{{ $item->company }}</th>
+                            <td>{{ $item->departure_station }}</td>
+                            <td>{{ $item->departure_date }}</td>
+                            <td>{{ $item->departure_time }}</td>
+                            <td>{{ $item->arrival_station }}</td>
+                            <td>{{ $item->arrival_date }}</td>
+                            <td>{{ $item->arrival_time }}</td>
+                            <td>{{ $item->train_code }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </main>
 
 @endsection
